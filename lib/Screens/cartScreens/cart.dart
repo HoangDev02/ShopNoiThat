24a887bc24b4c413a7f25410529451e68c688b83
Le Canh Hoang <hoang52287@donga.edu.vn>
2022-12-05 16:22:36 +0700

    note

--- lib/Screens/cartScreens/cart.dart
+++ lib/Screens/cartScreens/cart.dart
@@ -1,12 +1,45 @@
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Cart extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Container(
-
+      padding: EdgeInsets.all(20),
+      child: Row(
+        children: [
+          Container(
+            margin: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+            padding: EdgeInsets.all(20),
+            height: 150,
+            width: 331,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20)
+            ),
+            child: Row(
+              children: [
+                Container(
+                  height: 150,
+                  width: 100,
+                  decoration: BoxDecoration(
+                      color: Colors.black12,
+                      borderRadius: BorderRadius.circular(20)
+                  ),
+                  // child: Image(Ima),
+                )
+              ],
+            ),
+          ),
+          // Stack(
+          //   children: [
+          //
+          //   ],
+          // )
+        ],
+      ),
     );
   }
 }
--- lib/pages/CartPages.dart
+++ lib/pages/CartPages.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shopnoithat/Screens/cartScreens/cart.dart';
 
 class CartPages extends StatelessWidget{
+  // final int _while10 = F3F3F3FF;
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-
+      backgroundColor: Color.fromARGB(238, 243, 243, 243),
+      appBar: AppBar(
+        title: Text("My Cart"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Cart(),
+          ],
+        ),
+      ),
     );
   }
 }
